42e87635d475aba02bd4faa37ff6df65d0de1134
fitra.khasyah <fitra.khasyah@ipc.co.id>
2025-08-20 15:12:45 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const PORT = 18201
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Khasyahfr', description: "Welcome to khasyahfr's space"})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
